7a3bc7c1c185baeb2df4b274b77d20f67acbd143
mnoun09 <monikanoun2009@gmail.com>
2025-12-10 17:11:29 -0800

    Add meteor animation and collision detection
    - When player interacts w meteor, the terminal prints out a message
    - Couldn't figure out how to add gravity to the meteor... will add next time

---
+++ meteor.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+@onready var animated_sprite = $AnimatedSprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	animated_sprite.play("default")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+	pass
+
+func _on_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	print ("player hit")
+	pass # Replace with function body.
